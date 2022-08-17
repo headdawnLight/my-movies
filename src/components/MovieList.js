d77e19a0384d7daba964c18e2bdf0a13c2196a96
Headdawn Light <sertanix@yandex.com>
2022-08-17 23:49:47 +0300

    Add OMDb API to MovieList component and fix style

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,9 +1,46 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import "../styles/MovieList.css";
 
 const MovieList = () => {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    let movieBox = [];
+    const myMovies = [
+      "tt0068646",
+      "tt1345836",
+      "tt0110912",
+      "tt0137523",
+      "tt1375666",
+      "tt0099685",
+      "tt0816692",
+      "tt0114369",
+      "tt0407887",
+      "tt0078788",
+      "tt0482571",
+      "tt1853728",
+    ];
+
+    myMovies.forEach((movie) => {
+      (async () => {
+        const response = await axios.get(
+          `https://www.omdbapi.com/?i=${movie}&apikey=187ccaf5`
+        );
+        movieBox = [...movieBox, response.data];
+        setMovies(movieBox);
+      })();
+    });
+  }, []);
+
   return (
     <section className="App-section">
-      <div className="section-container"></div>
+      <div className="movie-list">
+        {movies.map((mov) => (
+          <div className="movie-box" key={mov.imdbID}>
+            <img src={mov.Poster} alt="movie_img" />
+          </div>
+        ))}
+      </div>
     </section>
   );
 };

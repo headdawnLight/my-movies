59afad88b3862510609937a554345ea5ecf5e80b
Headdawn Light <sertanix@yandex.com>
2022-08-22 23:57:14 +0300

    Implement redis client at rest api

--- api/app.js
+++ api/app.js
@@ -1,23 +1,32 @@
-const express = require("express");
-const app = express();
-const axios = require("axios");
-const bodyParser = require("body-parser");
+// import required packages
 require("dotenv").config();
+const express = require("express");
+const axios = require("axios");
+const redis = require("redis");
 
-app.use(bodyParser.urlencoded({ extended: true }));
+// express server
+const app = express();
 
-app.get("/api/search", async (req, res) => {
+// redis client
+const client = redis.createClient();
+client.on("error", (err) => console.log("Redis Client Error", err));
+client.connect();
+
+// request data from omdb
+const getData = async (req, res) => {
   const searchTerm = req.query.q;
   const api_URL = `http://www.omdbapi.com/?s=${searchTerm}&apikey=${process.env.OMDB_API_KEY}`;
-
   await axios
     .get(api_URL)
     .then((response) => {
-      res.json(response.data);
+      res.json(response.data.Search);
     })
     .catch((error) => {
       console.log(error);
+      res.status(500);
     });
-});
+};
+
+app.get("/api/search", getData);
 
 module.exports = app;

a116c1f341c078fe2b76dcf74909fe7e72dedcc3
Headdawn Light <sertanix@yandex.com>
2022-08-24 11:36:39 +0300

    Build redis caching at rest api

--- api/app.js
+++ api/app.js
@@ -7,26 +7,72 @@ const redis = require("redis");
 // express server
 const app = express();
 
-// redis client
-const client = redis.createClient();
-client.on("error", (err) => console.log("Redis Client Error", err));
-client.connect();
+// redis client on connect
+let redisClient;
 
-// request data from omdb
-const getData = async (req, res) => {
+(async () => {
+  redisClient = redis.createClient();
+
+  redisClient.on("error", (error) => console.error(`Error : ${error}`));
+
+  await redisClient.connect();
+})();
+
+// fetch data from omdb api
+async function fetchApiData(searchTerm) {
+  const apiResponse = await axios.get(
+    `http://www.omdbapi.com/?s=${searchTerm}&apikey=${process.env.OMDB_API_KEY}`
+  );
+  console.log("Request sent to the API");
+  return apiResponse.data;
+}
+
+// get movies data from omdb api
+async function getMoviesData(req, res) {
   const searchTerm = req.query.q;
-  const api_URL = `http://www.omdbapi.com/?s=${searchTerm}&apikey=${process.env.OMDB_API_KEY}`;
-  await axios
-    .get(api_URL)
-    .then((response) => {
-      res.json(response.data.Search);
-    })
-    .catch((error) => {
-      console.log(error);
-      res.status(500);
-    });
-};
+  let searchQuery;
 
-app.get("/api/search", getData);
+  try {
+    searchQuery = await fetchApiData(searchTerm);
+    if (searchQuery.length === 0) {
+      throw "API returned an empty array";
+    }
+    await redisClient.set(searchTerm, JSON.stringify(searchQuery), {
+      EX: 300,
+      NX: true,
+    });
+
+    res.send({
+      fromCache: false,
+      results: searchQuery,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(404).send("Data unavailable");
+  }
+}
+
+// cache middleware
+async function cacheData(req, res, next) {
+  const searchTerm = req.query.q;
+  let searchQuery;
+  try {
+    const cacheQuery = await redisClient.get(searchTerm);
+    if (cacheQuery) {
+      searchQuery = JSON.parse(cacheQuery);
+      res.send({
+        fromCache: true,
+        results: searchQuery,
+      });
+    } else {
+      next();
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(404);
+  }
+}
+
+app.get("/api/search", cacheData, getMoviesData);
 
 module.exports = app;

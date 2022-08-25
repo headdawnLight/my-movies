f28e086aacce7b0ea98f588cdad779bee35e70f4
Headdawn Light <sertanix@yandex.com>
2022-08-25 20:27:49 +0300

    Import MovieContext to MovieList

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,45 +1,30 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useContext } from "react";
+import { MovieContext } from "../contexts/MovieContext";
 import "../styles/MovieList.css";
 
 const MovieList = () => {
-  const [movies, setMovies] = useState([]);
-  useEffect(() => {
-    let movieBox = [];
-    const myMovies = [
-      "tt0068646",
-      "tt1345836",
-      "tt0110912",
-      "tt0137523",
-      "tt1375666",
-      "tt0099685",
-      "tt0816692",
-      "tt0114369",
-      "tt0407887",
-      "tt0078788",
-      "tt0482571",
-      "tt1853728",
-    ];
-
-    myMovies.forEach((movie) => {
-      (async () => {
-        const response = await axios.get(
-          `https://www.omdbapi.com/?i=${movie}&apikey=187ccaf5`
-        );
-        movieBox = [...movieBox, response.data];
-        setMovies(movieBox);
-      })();
-    });
-  }, []);
-
+  const { data } = useContext(MovieContext);
   return (
     <section className="App-section">
       <div className="movie-list">
-        {movies.map((mov) => (
-          <div className="movie-box" key={mov.imdbID}>
-            <img src={mov.Poster} alt="movie_img" />
-          </div>
-        ))}
+        {data === "Movie is not found!" ? (
+          <h2>Movie is not found!</h2>
+        ) : (
+          data.map((movie) => (
+            <div className="movie-box" key={movie.imdbID}>
+              <div className="movie-img">
+                {movie.Poster === "N/A" ? (
+                  <img
+                    src="https://www.prokerala.com/movies/assets/img/no-poster-available.jpg"
+                    alt="N/A"
+                  />
+                ) : (
+                  <img src={movie.Poster} alt="movie_img" />
+                )}
+              </div>
+            </div>
+          ))
+        )}
       </div>
     </section>
   );

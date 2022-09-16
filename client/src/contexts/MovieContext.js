b7e7eb01325d320e18587df08ff3d452da645f40
Headdawn Light <sertanix@yandex.com>
2022-09-16 10:04:32 +0300

    Set debounce time for axios requests

--- client/src/contexts/MovieContext.js
+++ client/src/contexts/MovieContext.js
@@ -4,32 +4,48 @@ import axios from "axios";
 export const MovieContext = createContext();
 
 const MovieContextProvider = (props) => {
+  // create context states
   const [data, setData] = useState([]);
   const [search, setSearch] = useState("the matrix");
 
   useEffect(() => {
+    // set controller to cancel axios request
     const controller = new AbortController();
     const signal = controller.signal;
-    const fetchData = async () => {
-      await axios
-        .get(`/api/search?q=${search}`, { signal })
-        .then((response) => {
-          const trueOrFalse = response.data.results.Response;
-          if (trueOrFalse !== "False") {
-            setData(response.data.results.Search);
-          } else {
-            setData("Movie is not found!");
-          }
-          console.log(response.data.results);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+
+    // fetch data from rest api
+    const fetchData = () => {
+      // set debounce time for axios requests
+      const timeout = setTimeout(() => {
+        axios
+          .get(`/api/search?q=${search}`, { signal })
+          .then((response) => {
+            // is response true or false?
+            const trueOrFalse = response.data.results.Response;
+            if (trueOrFalse !== "False") {
+              setData(response.data.results.Search);
+            } else {
+              setData("Movie is not found!");
+            }
+            console.log(response.data.results);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }, 400); // timeout of 400 milliseconds
+
+      // clear the timeout
+      return () => clearTimeout(timeout);
     };
+
+    // invoke the function
     fetchData();
+
+    // cleanup the function
     return () => controller.abort();
   }, [search]);
 
+  // set minimum search term length
   const onChangeHandler = (event) => {
     event.target.value.length >= 3
       ? setSearch(event.target.value)

bb694277d5210b0852492b4dee249b9113b1b776
Headdawn Light <sertanix@yandex.com>
2022-09-18 23:58:07 +0300

    Implement nock and supertest to api test

--- server/tests/app.js
+++ server/tests/app.js
@@ -1,37 +1,35 @@
 //required packages
 const chai = require("chai");
-const should = chai.should();
-const chaiHttp = require("chai-http");
-
-chai.use(chaiHttp);
+const nock = require("nock");
+const expect = require("chai").expect;
+var request = require("supertest")("http://localhost:5000");
 
 describe("Send request to search movie in api", () => {
   it("Search existent movie, should return response true!", (done) => {
-    //assertion
-    chai
-      .request("http://localhost:5000")
-      .get("/api/search?q=titanic")
-      .end((err, res) => {
-        should.not.exist(err);
-        should.exist(res);
-        res.should.have.status(200);
-        res.body.results.Response.should.equal("True");
-        done();
-      });
+    //setup mocked backend for a specific end point
+    nock("http://localhost:5000").get("/api/search?q=titanic").reply(200, {
+      Response: "True",
+    });
+
+    //perform the request to the api which will now be intercepted by nock
+    request.get("/api/search?q=titanic").end(function (err, res) {
+      expect(res.body.Response).to.equal("True");
+      done();
+    });
   });
 
   it("Search non-existent movie, should return response false!", (done) => {
-    //assertion
-    chai
-      .request("http://localhost:5000")
-      .get("/api/search?q=asdf123")
-      .end((err, res) => {
-        should.not.exist(err);
-        should.exist(res);
-        res.should.have.status(200);
-        res.body.results.Response.should.equal("False");
-        res.body.results.Error.should.equal("Movie not found!");
-        done();
-      });
+    //setup mocked backend for a specific end point
+    nock("http://localhost:5000").get("/api/search?q=asdf123").reply(200, {
+      Response: "False",
+      Error: "Movie not found!",
+    });
+
+    //perform the request to the api which will now be intercepted by nock
+    request.get("/api/search?q=asdf123").end(function (err, res) {
+      expect(res.body.Response).to.equal("False");
+      expect(res.body.Error).to.equal("Movie not found!");
+      done();
+    });
   });
 });

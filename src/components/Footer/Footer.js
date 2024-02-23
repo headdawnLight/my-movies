37b8e62e9a5257c508e167c685fcdd4d72aef433
Headdawn Light <headdawn.light@outlook.com>
2024-02-23 23:49:17 +0300

    Update footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,14 @@
-import "../styles/Footer.css";
+import "./Footer.css";
 
 const Footer = () => {
+  // get current year for copyright symbol
+  const getCurrentYear = () => {
+    return new Date().getFullYear();
+  };
+
   return (
     <footer className="App-footer">
-      <p className="footer-text">©2022 Headdawn Light. All rights reserved.</p>
+      <p className="footer-text">©{getCurrentYear()} Headdawn Light. All rights reserved.</p>
       <a href="https://github.com/headdawnLight" className="footer-link">
         <span id="footer-link-text">@myGitHub</span>
       </a>
